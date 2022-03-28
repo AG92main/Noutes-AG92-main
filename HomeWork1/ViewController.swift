b2fc9ec6552791ce58891261bf87f3fcedfe1492
AG92main <102309096+AG92main@users.noreply.github.com>
2022-03-28 18:31:17 +0300

    Initial commit

---
+++ HomeWork1/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  HomeWork1
+//
+//  Created by Andrey on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var TextViewNotes: UITextView!
+    
+    @IBOutlet weak var titleView: UITextField!
+    
+   
+    @IBOutlet weak var titeleButtom: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()}
+        // Do any additional setup after loading the view.
+        
+      
+        
+    }
+    
+    
+        
+        
+        
+        
+    
+  
+    
+    
+
+
